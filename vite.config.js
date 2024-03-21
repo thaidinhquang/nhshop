d20cc911404a2d5363fea85bdd094be7c8d005ff
thaidinhquang <cogaiphuoclanh96@gmail.com>
2024-03-21 13:39:18 +0700

    update1

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        detail: resolve(__dirname, "./detail.html"),
-        cart: resolve(__dirname, "./cart.html"),
-        checkout: resolve(__dirname, "./checkout.html"),
-        login: resolve(__dirname, "./login.html"),
-        shop: resolve(__dirname, "./shop.html"),
+        detail: resolve(__dirname, "./detail/detail.html"),
+        cart: resolve(__dirname, "./cart/cart.html"),
+        checkout: resolve(__dirname, "./checkout/checkout.html"),
+        login: resolve(__dirname, "./login/login.html"),
+        shop: resolve(__dirname, "./shop/shop.html"),
       },
     },
   },
