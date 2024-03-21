65342860a1eae3b16c00887597c0e342cd7ec6a1
thaidinhquang <cogaiphuoclanh96@gmail.com>
2024-03-21 13:37:48 +0700

    update1

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        shop: resolve(__dirname, "./shop.html"),
         detail: resolve(__dirname, "./detail.html"),
         cart: resolve(__dirname, "./cart.html"),
         checkout: resolve(__dirname, "./checkout.html"),
         login: resolve(__dirname, "./login.html"),
+        shop: resolve(__dirname, "./shop.html"),
       },
     },
   },
